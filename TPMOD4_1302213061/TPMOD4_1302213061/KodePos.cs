82e2600d6f51fc22c89be3256d23d7d66ba75fc0
iqrobanyuanto <iqrobanyuanto12345@gmail.com>
2023-03-11 13:59:31 +0700

    Menambahkan Implementasi dengan table driven

---
+++ TPMOD4_1302213061/TPMOD4_1302213061/KodePos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class KodePos
+{
+	private enum Kelurahan{Batununggal, Kujangsari,Mengger,Wates,Cijaura,Jatisari,Margasari,Sekejati,Kebonwaru,Maleer,Samoja}
+	private int[] Kode = { 40266 , 40287 , 40267 , 40256 , 40287 , 40286 , 40286 , 40286 , 40272 , 40274 , 40273 };
+	public void GetKodePos()
+	{
+		for(int i = 0; i < Kode.Length; i++)
+		{
+			Console.WriteLine((Kelurahan)i + " " + Kode[i]);
+		}
+	}
+}
