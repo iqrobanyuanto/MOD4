43e704c47cf8bc9541a3f7413f9ec35dea409ea0
iqrobanyuanto <iqrobanyuanto12345@gmail.com>
2023-03-12 22:48:05 +0700

    menambahkan implementasi dengan state-based construction

---
+++ TPMOD4_1302213061/TPMOD4_1302213061/DoorMachine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.Versioning;
+using System.Transactions;
+
+public class DoorMachine
+{	
+	//Define attribute
+	public enum StateMasuk {Terkunci, Terbuka}
+	public enum StateAction {Buka_Pintu,Kunci_Pintu}
+	
+	public StateMasuk prev;
+	public StateAction act;
+	public StateMasuk currentState;
+
+	public DoorMachine(StateMasuk awal)
+	{
+		currentState = awal;
+    }
+	public StateMasuk door(StateMasuk prev, StateAction act)
+	{
+		StateMasuk nextState = prev;
+		if(prev == StateMasuk.Terkunci && act == StateAction.Buka_Pintu) {
+			nextState = StateMasuk.Terbuka;
+		}else if(prev == StateMasuk.Terbuka && act == StateAction.Kunci_Pintu)
+		{
+			nextState = StateMasuk.Terkunci;
+		}
+
+		return nextState;
+
+	}
+
+	public void trigger(StateAction act)
+	{
+		currentState = prev;
+		StateMasuk next = door(currentState, act);
+        currentState = next;
+    }
+
+	public void cekState()
+	{
+		Console.WriteLine(currentState);
+	}
+}
--- TPMOD4_1302213061/TPMOD4_1302213061/Program.cs
+++ TPMOD4_1302213061/TPMOD4_1302213061/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 KodePos pos = new KodePos();
 pos.GetKodePos();
+Console.WriteLine("\n");
+DoorMachine door = new DoorMachine(DoorMachine.StateMasuk.Terbuka);
+door.cekState();
+door.trigger(DoorMachine.StateAction.Kunci_Pintu);
+door.cekState();
